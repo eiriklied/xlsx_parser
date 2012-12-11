afcf5a55c8f7afc32c8b0acc7a4076e1d351a62c
Eirik Lied <eiriklied@gmail.com>
2012-12-11 21:34:42 +0100

    test some calls to empty cells

--- spec/sheet_spec.rb
+++ spec/sheet_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe Sheet do
 
-  it 'should answer to cell calls' do
+  before do
     @shared_strings = ['Cell 1 A', 'Cell 2 B', 'Cell 2 C', 'Dato']
     @sheet = Sheet.new('test', 'data/sheet1.xml', @shared_strings)
-    @sheet.cell(1,'A')
-    @sheet.cell(1,1).should == 'Cell 1 A'
+  end
+
+  it 'should answer to cell calls for existing cells' do
+    @sheet.cell(1,1).should eq('Cell 1 A')
+    @sheet.cell(1,'A').should eq('Cell 1 A')
+    
+    @sheet.cell(2, 2).should eq('Cell 2 B')
+    @sheet.cell(2, 'B').should eq('Cell 2 B')
+  end
+
+  it 'should should return nil for non-existing cells' do
+    @sheet.cell(-1, -1).should be_nil
+    @sheet.cell(0,0).should be_nil
+    @sheet.cell(2,1).should be_nil
   end
 end
